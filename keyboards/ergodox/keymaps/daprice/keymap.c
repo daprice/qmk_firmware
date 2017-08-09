d1db0243acf98f1f1a7d0f0dabdd335517d6bc73
Dale Price <daprice@mac.com>
2017-08-08 20:03:51 -0500

    Move reset key to media layer; allow access to media layer from numpad layer; move tilde and backslash to a more comfortable position

--- keyboards/ergodox/keymaps/daprice/keymap.c
+++ keyboards/ergodox/keymaps/daprice/keymap.c
@@ -11,17 +11,6 @@
 #define NUMP 3 // numpad
 #define MEDI 4 // media controls
 
-//tap dance declarations
-enum {
-	TD_TILDE_CAPS = 0
-};
-
-//tap dance definitions
-qk_tap_dance_action_t tap_dance_actions[] = {
-	//tap once for tilde, twice for caps lock
-	[TD_TILDE_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_GRV, KC_CAPSLOCK)
-};
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
@@ -34,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------| ~L1  |           | ~L1  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |~/Caps|  \   | LCtl | LAlt | LGui |                                       |←/RGui|↓/RAlt|↑/RCtl|  →   |MEDIA/Mute
+ *   | Caps |  ~   |\/LCtl| LAlt | LGui |                                       |←/RGui|↓/RAlt|↑/RCtl|  →   |MEDIA/Mute
  *   `----------------------------------'                                       `---------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | LCtl | LAlt |       | RAlt |RCtrl |
@@ -52,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_BSLS,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(UTIL),
         KC_TAB,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   MO(UTIL),
-        TD(TD_TILDE_CAPS),KC_BSLS,      KC_LCTL,  KC_LALT,KC_LGUI,
+        KC_CAPS,KC_GRV,  LCTL_T(KC_BSLS),  KC_LALT,KC_LGUI,
                                               KC_LCTL,  KC_LALT,
                                                               KC_HOME,
                                                KC_BSPC,KC_DEL,KC_END,
@@ -69,7 +58,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 1: Function + Vim Arrows Layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * | TEENSY |  F1  |  F2  |  F3  |  F4  |  F5  |      |           | GAME |  F6  |  F7  |  F8  |  F9  |  F10 |        |
+ * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           | GAME |  F6  |  F7  |  F8  |  F9  |  F10 |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |   (  |      |      |      | F11  |  F12 |   )    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -90,7 +79,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Functions and arrows
 [UTIL] = KEYMAP(
        // left hand
-       RESET,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
+       KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_LEFT,KC_UP,KC_DOWN,KC_RGHT,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -119,7 +108,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |  1   |  2   |  3   | ENT  |  TAB   |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |   0  |  0   |  .   | ENT  |  TAB |
+ *   |      |      |      |      |      |                                       |   0  |  0   |  .   | ENT  |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -145,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS, KC_TRNS, KC_P7,     KC_P8,      KC_P9,      KC_PMNS,  KC_TAB,
                 KC_TRNS, KC_P4,     KC_P5,      KC_P6,      KC_PPLS,  KC_TAB,
        KC_TRNS, KC_TRNS, KC_P1,     KC_P2,      KC_P3,      KC_PENT,  KC_TAB,
-                         KC_P0,     KC_P0,      KC_PDOT,    KC_PENT,  KC_TAB,
+                         KC_P0,     KC_P0,      KC_PDOT,    KC_PENT,  KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
@@ -153,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 3: Media controls
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |        |      |      |      |      |      | PWR  |           |      |      |      |      |      |      |        |
+ * | TEENSY |      |      |      |      |      | PWR  |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -174,7 +163,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Media
 [MEDI] = KEYMAP(
        // left hand
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_POWER,
+       RESET,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_POWER,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -220,7 +209,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(UTIL),
         KC_CAPS,         KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   MO(UTIL),
-        TD(TD_TILDE_CAPS),KC_BSLS,      KC_LCTL,  KC_LALT,KC_SPC,
+         KC_CAPS,KC_GRV,  LCTL_T(KC_BSLS),  KC_LALT,KC_SPC,
                                               KC_LCTL,  KC_LALT,
                                                               KC_HOME,
                                                KC_BSPC,KC_DEL,KC_END,
